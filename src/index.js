3a6f1eebfe8188d8764b5217dfb3bcfe64688493
zcmarcus <zcm.box@gmail.com>
2021-09-12 12:21:52 -0500

    Add missing  vertical winning line [0, 3, 6]

--- src/index.js
+++ src/index.js
@@ -219,12 +219,14 @@ ReactDOM.render(
 function calculateWinner(squares) {
 
 	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
+
+		[0, 1, 2], /* Horizontal winning lines */
+		[3, 4, 5], 
+		[6, 7, 8],  
+		[0, 3, 6], /* Vertical winning lines */
 		[1, 4, 7],
 		[2, 5, 8],
-		[0, 4, 8],
+		[0, 4, 8], /* Diagonal winning lines */
 		[2, 4, 6],
 	];
 
